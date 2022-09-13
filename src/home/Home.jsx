476e4844186543395c529a6d8fcb9621597de05d
aydinkuru <aydinkuru81@gmail.com>
2022-09-13 23:59:38 +0200

    s.2

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,6 +5,11 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
       praesentium deserunt deleniti, a perferendis amet, accusantium dolor
       corrupti vel quasi molestias est repellat natus incidunt impedit assumenda
